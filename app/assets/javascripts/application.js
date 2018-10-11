5f27b27d4325201ed8917b6cffaa91ba15e21dc8
tkdtktd <tkdtktd3104@gmail.com>
2018-10-12 02:52:09 +0900

    deal with logout error 3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= jquery3
+//= jquery
+//= require rails-ujs
 //= require popper
 //= require bootstrap-sprockets
-//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
